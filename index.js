01a2b3981000cb7eebbe14e1c90bfd0365ca59dd
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2023-12-28 12:40:26 -0500

    added course

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const path = require("path");
 const pathToFile = path.resolve("./data/resources.json");
 const app = express();
 const PORT = 3001;
-
+const cors = require("cors");
 const fs = require("fs");
+
+let corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
 
 const getResource = () => JSON.parse(fs.readFileSync(pathToFile));
 
+app.use(cors(corsOptions));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
