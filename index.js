64f909b3daf0178f1d59cba2a8cff98cb14f16fc
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2024-01-02 19:41:31 -0500

    completed updating resource endpoint

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let corsOptions = {
 const getResource = () => JSON.parse(fs.readFileSync(pathToFile));
 
 app.use(cors(corsOptions));
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -21,6 +22,42 @@ app.get("/", (req, res) => {
 app.get("/api/resources", (req, res) => {
   const resource = getResource();
   res.send(resource);
+});
+
+app.get("/api/resources/:id", (req, res) => {
+  const resources = getResource();
+  const { id } = req.params;
+  const resource = resources.find((resource) => resource.id === id);
+  res.send(resource);
+});
+
+app.post("/api/resources", (req, res) => {
+  const resources = getResource();
+  const resource = req.body;
+  resource.id = Date.now().toString();
+  resource.createdAt = new Date();
+  resource.status = "inactive";
+  resources.unshift(resource);
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
+    if (err) {
+      return res.status(422).send("Cannot store data in file");
+    }
+    return res.send("Data has been saved");
+  });
+});
+
+app.patch("/api/resources/:id", (req, res) => {
+  const resources = getResource();
+  const { id } = req.params;
+  const index = resources.findIndex((resource) => resource.id === id);
+  resources[index] = req.body;
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
+    if (err) {
+      return res.status(422).send("Cannot store data in file");
+    }
+    return res.send("Resource has been updated");
+  });
 });
 
 app.listen(PORT, () => {
